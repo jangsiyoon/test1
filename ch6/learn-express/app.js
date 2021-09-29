acdfa2b9cd1aa34fd95d5e7e151c32baf49a27f3
jang si yoon <siyoon7600@naver.com>
2021-09-29 15:03:24 +0900

    1

--- ch6/learn-express/app.js
+++ ch6/learn-express/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
-app.set('port',process.env.PORT || 3000);
+const app = express(); // express를 실행시키고 별칭으로 app
+
+
+
+app.set('port', process.env.PORT || 3000); //json 요청을 받겠다.
 
 app.get('/', (rep, res) => {
    // res.send('hello express');
-   res.sendFile(path.join(__dirname,'/index.html'));
+   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(app.get('port'),() => {
